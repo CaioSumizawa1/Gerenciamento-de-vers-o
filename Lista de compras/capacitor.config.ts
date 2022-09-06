db2dde7004b252865d4df47fb63962ee53ae89da
CaioSumizawa1 <105085021+CaioSumizawa1@users.noreply.github.com>
2022-09-06 20:34:53 -0300

    lismercadopro

--- Lista de compras/capacitor.config.ts
+++ Lista de compras/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'listadecompra',
+  appName: 'lista_compras',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- Lista de compras/src/app/home/home.page.ts
+++ Lista de compras/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
 
 @Component({
   selector: 'app-home',
@@ -7,28 +9,59 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private storage:Storage) {
+    this.storage.create();
+  }
+
+  ngOnInit(){
+    this.atualizaLista();
+  }
 
   variavel_lista = [];
   texto: string = "";
+  preco: number = 0;
+  total: number = 0;
+  aux = 0;
 
-  adiciona() {
-    if (!(this.texto == "")) {
-      this.variavel_lista.push(this.texto);
+  async adiciona() {
+    if (!(this.texto == "" || this.preco == 0)) {
+
+      this.variavel_lista.forEach(item => {
+        if(parseInt(item[0]) > this.aux) {
+          this.aux = parseInt(item[0]);
+        }
+      })
+      this.aux = this.aux + 1;
+      await this.storage.set(this.aux.toString(), [this.texto, this.preco]);
+      this.atualizaLista();
       this.texto = "";
+      this.preco = 0;
+    
+      
     }
+    
+     
 
-      /*
-    if (this.texto == "") {
-
-    } else{
-      this.variavel_lista.push(this.texto);
-      this.texto = "";
-    }*/
-   
   }
 
-  //*ngFor = "let elemento_da_lista of minhaLista" no item
-  //[(ngModel)]="texto" no input
+  atualizaLista() {
+    this.variavel_lista = [];
+    this.total = 0;
+    this.storage.forEach((value, key, index) => {
+      this.variavel_lista.push([key, value]);
+      this.soma(value[1]);
+    })
+  }
 
+  async remove(indice) {
+    
+    await this.storage.remove(indice);
+    this.atualizaLista();
+
+  
+ 
+}
+soma(preco){
+  this.total = (+this.total) + (+preco)
+}
 }
